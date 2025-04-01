741a8efa30c1a2511ff17aac634717e49fdf7e68
Andershgras <150132950+Andershgras@users.noreply.github.com>
2025-04-01 12:07:50 +0200

    Add files via upload

---
+++ Pages/Items/GetAllItems.cshtml.cs
@@ -0,0 +1,22 @@
+using Bookingsystem.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bookingsystem.Pages.Items
+{
+    public class GetAllItemsModel : PageModel
+    {
+        public ItemService _itemService; //Instancefield
+        public List<Models.Item> Items { get; set; }
+        public void OnGet()
+        {
+            Items = MockData.MockItems.GetMockItems();
+            Items = _itemService.GetItems(); //Refaktorer onGet() metoden så den henter Mock-Data via servicen istedet for direkte via GetAllItems()
+        }
+        public GetAllItemsModel(ItemService itemService) //Dependency Injection
+        {
+            this._itemService = itemService;
+        }
+    }
+}
+ 
